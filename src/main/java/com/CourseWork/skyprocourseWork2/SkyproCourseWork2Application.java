ba3d629407a63e326227e54001f06eea0d8462a9
Александр <alexst731w@gmail.com>
2023-10-15 14:16:15 +0300

    commit-1

---
+++ src/main/java/com/CourseWork/skyprocourseWork2/SkyproCourseWork2Application.java
@@ -0,0 +1,14 @@
+package com.CourseWork.skyprocourseWork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCourseWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproCourseWork2Application.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/CourseWork/skyprocourseWork2/SkyproCourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CourseWork.skyprocourseWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
